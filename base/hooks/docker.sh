a48d2d63521411ddedafd80643cf66d18157caea
Keven - Yannic Iwansky <keven.iwansky@gmail.com>
2026-05-11 15:07:08 +0200

    feat: switch base from docker to podman

--- base/hooks/docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Enable and start the docker service
-sudo systemctl enable --now docker
-
-# Add current user to the docker group
-sudo usermod -aG docker "$USER"
